56841698722bbea316a11f2b9356aec9da052e62
AdrianRudeanu <48693528+AdrianRudeanu@users.noreply.github.com>
2020-11-30 19:08:07 +0200

    -

--- tema2_2/lib/main.dart
+++ tema2_2/lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Guess the number',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Guess the number'),
     );
   }
 }
