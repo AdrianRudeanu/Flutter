a0c6b0a204c328012043f7546b6f5e195c0260ba
AdrianRudeanu <48693528+AdrianRudeanu@users.noreply.github.com>
2020-12-07 15:05:31 +0200

    tema3_1

---
+++ tema3_1/lib/main.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: MyHomePage(title: 'tic-tac-toe'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isRed = true;
+  bool hasWon = false;
+  List<String> gameState;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      this.gameState = [
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+      ];
+    });
+  }
+
+  void playGame(int index) {
+    if (this.gameState[index] == "empty") {
+      setState(() {
+        if (this.isRed) {
+          this.gameState[index] = "red";
+        } else {
+          this.gameState[index] = "green";
+        }
+        this.isRed = !this.isRed;
+        this.checkWin();
+      });
+    }
+  }
+
+  void resetGame() {
+    setState(() {
+      this.gameState = [
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+        "empty",
+      ];
+    });
+  }
+
+  void checkWin() {
+    if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[1]) &&
+        (gameState[1] == gameState[2])) {
+      // if any user Win update the message state
+      setState(() {
+        hasWon = true;
+      });
+    } else if ((gameState[3] != 'empty') &&
+        (gameState[3] == gameState[4]) &&
+        (gameState[4] == gameState[5])) {
+      setState(() {
+        hasWon = true;
+      });
+    } else if ((gameState[6] != 'empty') &&
+        (gameState[6] == gameState[7]) &&
+        (gameState[7] == gameState[8])) {
+      setState(() {
+        hasWon = true;
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[3]) &&
+        (gameState[3] == gameState[6])) {
+      setState(() {
+        hasWon = true;
+      });
+    } else if ((gameState[1] != 'empty') &&
+        (gameState[1] == gameState[4]) &&
+        (gameState[4] == gameState[7])) {
+      setState(() {
+        hasWon = true;
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[5]) &&
+        (gameState[5] == gameState[8])) {
+      setState(() {
+        hasWon = true;
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[4]) &&
+        (gameState[4] == gameState[8])) {
+      setState(() {
+        hasWon = true;
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[4]) &&
+        (gameState[4] == gameState[6])) {
+      setState(() {
+        hasWon = true;
+      });
+    } else if (!gameState.contains('empty')) {
+      setState(() {
+        hasWon = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0),
+              itemCount: this.gameState.length,
+              itemBuilder: (context, i) => GestureDetector(
+                  onTap: (){
+                    playGame(i);
+                  },
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                  )
+              ),
+            ),
+            Visibility(
+              visible: hasWon,
+              child:MaterialButton(
+                child: const Text(
+                  'Reset Game',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {
+                  resetGame();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
